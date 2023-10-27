b99678788af9393296de46ca8c10935f4203d2b6
Karoline Leite <147656869+karolineLeite351684@users.noreply.github.com>
2023-10-27 18:10:30 -0300

    add configuração de autenticação (#5)

---
+++ src/main/java/br/com/fiap/siges/dto/DadosAutenticacaoDTO.java
@@ -0,0 +1,5 @@
+package br.com.fiap.siges.dto;
+
+public record DadosAutenticacaoDTO(String login, String senha) {
+
+}
---
+++ src/main/java/br/com/fiap/siges/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package br.com.fiap.siges.security;
+
+public record DadosTokenJWT(String token) {
+}
